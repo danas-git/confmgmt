422b10144e21c907139f440dc9189c9bd26e13fa
Sheelendra Singh Chauhan <sheelendra.singh@stud.tu-darmstadt.de>
2016-08-27 17:45:45 +0200

    deleting

--- public/js/services/HomeServiceModule.js
+++
@@ -1,8 +0,0 @@
-angular.module('HomeServiceModule',[]).factory('homeService',['$http',function($http){
-    console.log("homeservice");
-    return {
-        logout : function(){
-            return $http.get("/users/signout");
-        }
-    }
-}]);
